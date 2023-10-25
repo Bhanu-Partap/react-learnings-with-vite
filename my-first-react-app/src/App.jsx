22bdcbf9d83cef16b1afcb515e52e855c6177ed1
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-25 15:06:42 +0530

    class component

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from "react"
+import React from "react"
 import "./App.css"
+// import { useState } from "react";
 
-export default function App() {
-  const [data, setData] = useState("M5")
 
-  function updateData() {
-    setData("BMW M5");
+// export default function App() {
+//   const [data, setData] = useState("M5")
+
+//   function updateData() {
+//     setData("BMW M5");
+//   }
+
+//   return (
+//     <>
+//       <h1>{data}</h1>
+//       <button onClick={updateData}>Update the data</button>
+//     </>
+//   )
+// }
+
+
+ class App extends React.Component {
+  render(){
+    return (
+      <>
+        <h1>Hello </h1>
+        <button>Update the data</button>
+      </>
+    )
   }
-
-  return (
-    <>
-      <h1>{data}</h1>
-      <button onClick={updateData}>Update the data</button>
-    </>
-  )
 }
+
+export default App ;
