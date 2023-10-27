7fdc2e46a663e8100c13736ba83efe61e41f2b41
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-27 17:08:13 +0530

    hide nd show elemnt on diff. buttons

--- my-first-react-app/src/Components/ToggleElement.jsx
+++ my-first-react-app/src/Components/ToggleElement.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import useState  from 'react'
+import {useState}  from 'react'
 
 export default function ToggleElement() {
     const [status, setStatus] = useState(true);
