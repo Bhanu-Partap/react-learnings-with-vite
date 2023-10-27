384e65f7301887a35fa3ce7d0a05e9e9858b4e01
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-27 15:08:11 +0530

    Commenting

--- my-first-react-app/src/Components/InputBox.jsx
+++ my-first-react-app/src/Components/InputBox.jsx
@@ -4,6 +4,25 @@ import { useState } from "react";
 
 export default function InputBox() {
 
+    //=============Normal Using state getting the value of the input box ============//
+    
+//     const [data, setData] = useState(null);
+
+//     function getData(val){
+//         console.log(val.target.value);
+//         setData(val.target.value)
+//     }
+//   return (
+//     <>
+//         <div className="inputbox">
+//             <h1>Input Box</h1>
+//             <input type="text" onChange={getData}/>
+//             <h2>The Input data is : {data}</h2>
+//         </div>
+//     </>
+//     )
+
+
     const [data, setData] = useState(null);
 
     function getData(val){
