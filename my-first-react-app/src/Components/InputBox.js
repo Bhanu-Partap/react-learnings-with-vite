03983b7e6af1f3a7d986e23d9d59ee8dd911fa30
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-27 14:48:29 +0530

    gete unput box data

---
+++ my-first-react-app/src/Components/InputBox.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+export default function InputBox() {
+  return (
+    <>
+        <div className="inputbox">
+            <h1>Input Box</h1>
+            <input type="text" />
+        </div>
+    </>
+    )
+}
