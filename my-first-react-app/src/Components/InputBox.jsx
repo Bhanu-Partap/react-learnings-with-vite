1ad84d208b2d8cfa65243632b76837c085f2cbbc
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-27 15:02:45 +0530

    onchanging state , get the data

--- my-first-react-app/src/Components/InputBox.jsx
+++ my-first-react-app/src/Components/InputBox.jsx
@@ -8,12 +8,14 @@ export default function InputBox() {
 
     function getData(val){
         console.log(val.target.value);
+        setData(val.target.value)
     }
   return (
     <>
         <div className="inputbox">
             <h1>Input Box</h1>
             <input type="text" onChange={getData}/>
+            <h2>The Input data is : {data}</h2>
         </div>
     </>
     )
