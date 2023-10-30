866cf05d208c9b12d7b36d0ce6815006a59ccced
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-30 17:05:11 +0530

    formbox

---
+++ my-first-react-app/src/Components/Formbox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Formbox() {
+  return (
+    <>
+    <div className="Formbox">
+        <h1>Handle Form in React</h1>
+        <form >
+            <input type="text" placeholder='name' />
+        </form>
+    </div>
+    </>
+  )
+}
