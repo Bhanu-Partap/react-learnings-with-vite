81f9b61e2dbab07ffe790d8058a6f774103f125d
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-30 17:08:05 +0530

    form box

--- my-first-react-app/src/Components/Formbox.jsx
+++ my-first-react-app/src/Components/Formbox.jsx
@@ -6,7 +6,13 @@ export default function Formbox() {
     <div className="Formbox">
         <h1>Handle Form in React</h1>
         <form >
-            <input type="text" placeholder='name' />
+            <input type="text" placeholder='name' /> <br /> <br />
+            <select>
+                <option value="">Marvel</option>
+                <option value="">DC</option>
+                <option value="">DC avanti</option>
+            </select> <br /><br />
+            <input type="checkbox"  /><span>Accept the Terms and Conditions</span>
         </form>
     </div>
     </>
