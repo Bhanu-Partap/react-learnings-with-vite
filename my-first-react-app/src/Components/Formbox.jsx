7057e5a0277c07ecaa10578b0f746bb903c305e3
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-30 17:19:50 +0530

    usestate defines

--- my-first-react-app/src/Components/Formbox.jsx
+++ my-first-react-app/src/Components/Formbox.jsx
@@ -1,6 +1,12 @@
 // import React from 'react's
 
+import { useState } from "react";
+
 export default function Formbox() {
+
+    const [name, setName] = useState("");
+    const [tnc, setTnc] = useState("");
+    const [interest, setInterest] = useState("");
 
     function changeData(e){
         e.preventDefault();
@@ -9,7 +15,7 @@ export default function Formbox() {
     <>
     <div className="Formbox">
         <h1>Handle Form in React</h1>
-        <form onSubmit={changeData} >
+        <form  onSubmit={changeData} >
             <input type="text" placeholder='name' /> <br /> <br />
             <select>
                 <option value="">Marvel</option>
