cc9a3489687a94251591459f2e64197f76be152c
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-30 17:20:51 +0530

    states defined

--- my-first-react-app/src/Components/Formbox.jsx
+++ my-first-react-app/src/Components/Formbox.jsx
@@ -5,27 +5,27 @@ import { useState } from "react";
 export default function Formbox() {
 
     const [name, setName] = useState("");
-    const [tnc, setTnc] = useState("");
+    const [tnc, setTnc] = useState(false);
     const [interest, setInterest] = useState("");
 
-    function changeData(e){
+    function changeData(e) {
         e.preventDefault();
     }
-  return (
-    <>
-    <div className="Formbox">
-        <h1>Handle Form in React</h1>
-        <form  onSubmit={changeData} >
-            <input type="text" placeholder='name' /> <br /> <br />
-            <select>
-                <option value="">Marvel</option>
-                <option value="">DC</option>
-                <option value="">DC avanti</option>
-            </select> <br /><br />
-            <input type="checkbox"  /><span>Accept the Terms and Conditions</span>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="Formbox">
+                <h1>Handle Form in React</h1>
+                <form onSubmit={changeData} >
+                    <input type="text" placeholder='name' /> <br /> <br />
+                    <select>
+                        <option value="">Marvel</option>
+                        <option value="">DC</option>
+                        <option value="">DC avanti</option>
+                    </select> <br /><br />
+                    <input type="checkbox" /><span>Accept the Terms and Conditions</span>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </>
+    )
 }
