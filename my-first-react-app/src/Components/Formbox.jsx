0c388c7434a435955429aad3c79a15f8a75b5e79
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-30 17:10:28 +0530

    formbox

--- my-first-react-app/src/Components/Formbox.jsx
+++ my-first-react-app/src/Components/Formbox.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 export default function Formbox() {
+
+    function changeData(e){
+        e.preventdefault;
+    }
+
   return (
     <>
     <div className="Formbox">
         <h1>Handle Form in React</h1>
-        <form >
+        <form onSubmit={changeData} >
             <input type="text" placeholder='name' /> <br /> <br />
             <select>
                 <option value="">Marvel</option>
@@ -13,6 +18,7 @@ export default function Formbox() {
                 <option value="">DC avanti</option>
             </select> <br /><br />
             <input type="checkbox"  /><span>Accept the Terms and Conditions</span>
+            <button type="submit">Submit</button>
         </form>
     </div>
     </>
