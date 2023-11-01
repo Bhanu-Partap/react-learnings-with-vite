bc11fa034faf984970e35d65ec3247ed9b9b80e6
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-01 11:50:44 +0530

    Ternairy opereator conditional check

--- my-first-react-app/src/Components/Profile.jsx
+++ my-first-react-app/src/Components/Profile.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 
 export default function Profile() {
-    const [logeedIn, setlogeedIn] = useState(false)
+    const [logeedIn, setlogeedIn] = useState(true)
   return (
     <>
         {logeedIn ? <h1>Profie Page rendered successfully</h1> : <h1>Not Loged In</h1>}
