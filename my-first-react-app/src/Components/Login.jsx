546313d9d63c0b6ed6080ca770b1a40019865a96
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-01 14:41:44 +0530

    state created for user verification

--- my-first-react-app/src/Components/Login.jsx
+++ my-first-react-app/src/Components/Login.jsx
@@ -3,33 +3,34 @@ import React, { useState } from 'react'
 export default function Login() {
     const [userId, setUserId] = useState("");
     const [password, setPassword] = useState("");
+    const [userErr, setUserErr] = useState(false);
 
-    function loginHandle(e){
+    function loginHandle(e) {
         e.preventDefault();
     }
 
-    function userHandler(e){
-        let item =e.target.value;
-        if(item.length <3){
-            console.warn("invalid");
+    function userHandler(e) {
+        let item = e.target.value;
+        if (item.length < 3) {
+            setUserErr(true);
         }
         console.warn(e.target.value.length);
     }
 
-  
-  
-  return (
-    <>
-        <div className="Login">
-            <form onSubmit={loginHandle} >
-                <h1>Login Details</h1>
-                <input type="text" placeholder='User Id'  onChange={userHandler}/>
-                <br /> <br />
-                <input type="text" placeholder='password' />
-                <br /><br />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    </>
-  )
+
+
+    return (
+        <>
+            <div className="Login">
+                <form onSubmit={loginHandle} >
+                    <h1>Login Details</h1>
+                    <input type="text" placeholder='User Id' onChange={userHandler} />
+                    <br /> <br />
+                    <input type="text" placeholder='password' />
+                    <br /><br />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </>
+    )
 }
