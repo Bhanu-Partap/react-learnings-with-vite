92adbbbb2f1983e2e0efadd6cfb34de039241c7a
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-01 11:48:19 +0530

    State added

--- my-first-react-app/src/Components/Profile.jsx
+++ my-first-react-app/src/Components/Profile.jsx
@@ -1,7 +1,12 @@
 // import React from 'react'
 
+import { useState } from "react"
+
 export default function Profile() {
+    const [logeedIn, setlogeedIn] = useState(false)
   return (
-    <div>Profile</div>
+    <>
+
+    </>
   )
 }
