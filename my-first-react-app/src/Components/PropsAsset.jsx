d6bb95f04e26dc8aa85eb975816a3ea2f67c9c66
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-02 17:19:31 +0530

    Commenting

--- my-first-react-app/src/Components/PropsAsset.jsx
+++ my-first-react-app/src/Components/PropsAsset.jsx
@@ -6,6 +6,10 @@ export default function PropsAsset(props) {
         <div className="PropsAsset">
             <h1>Props Asset</h1>
             <h2>hello </h2>
+          {/* /==== If you want to show the functionality defined in parent component after some time or have to check the states brfore hitting the props. You can use with arrow function */} 
+            <button onClick={()=>props.name()}>Call the data</button>  
+
+          {/* /==== If you want to show the functionality defined in parent component directly without any wait. You can use with arrow function */} 
             <button onClick={props.name}>Call the data</button>
         </div>
     </>
