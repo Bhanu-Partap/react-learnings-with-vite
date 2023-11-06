c11209b28491d5acd86dbe859d9ad4a76f27aec2
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-06 17:02:54 +0530

    Error: re rendering of element occuring

--- my-first-react-app/src/Components/ComponentDidMount.jsx
+++ my-first-react-app/src/Components/ComponentDidMount.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 export default class ComponentDidMount extends React.Component {
-    constructor(){
+    constructor() {
         super()
         console.warn("Constructor");
     }
-    componentDidMount(){
+    componentDidMount() {
         console.warn("ComponentDidMount");
     }
-  render() {
-    console.warn("Render");
-    return (
-      <div>
-        <h1>componentDidMount</h1>
-      </div>
-    )
-  }
+    render() {
+        console.warn("Render");
+        return (
+            <div className="Component">
+                <h1>componentDidMount</h1>
+            </div>
+        )
+    }
 }
