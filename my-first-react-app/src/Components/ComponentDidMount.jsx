8de957325c0439610e4b656953dfe504c498ae87
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-06 16:04:41 +0530

    cheking the calling of elements

--- my-first-react-app/src/Components/ComponentDidMount.jsx
+++ my-first-react-app/src/Components/ComponentDidMount.jsx
@@ -3,17 +3,17 @@ import React, { Component } from 'react'
 export default class ComponentDidMount extends Component {
     constructor(){
         super()
-        console.warn("constructor");
+        console.warn("Constructor");
     }
 
     componentDidMount(){
         console.warn("ComponentDidMount");
     }
   render() {
-
+    console.warn("Render");
     return (
       <div>
-
+        <h1>componentDidMount</h1>
       </div>
     )
   }
