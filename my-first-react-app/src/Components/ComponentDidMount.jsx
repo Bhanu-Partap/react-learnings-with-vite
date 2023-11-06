aa7027b0b1add09c8c75212fa0bd0cae32f25ccf
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-06 16:59:48 +0530

    re rendering of element occuring

--- my-first-react-app/src/Components/ComponentDidMount.jsx
+++ my-first-react-app/src/Components/ComponentDidMount.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class ComponentDidMount extends Component {
+export default class ComponentDidMount extends React.Component {
     constructor(){
         super()
         console.warn("Constructor");
