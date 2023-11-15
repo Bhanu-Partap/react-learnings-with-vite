5d4b0fed59de9cacd1edbb737536cbe5f7bb32d9
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-15 14:18:40 +0530

    error resolved

--- my-first-react-app/src/Components/ComponentWillUnmount.jsx
+++ my-first-react-app/src/Components/ComponentWillUnmount.jsx
@@ -2,18 +2,17 @@ import react , {Component} from "react"
 
 export default class ComponentWillUnmount extends Component{
     constructor(){
-        super(){
+        super()
             this.state= {
                 name:"Bhanu"
             }
-        }
-        render(){
-            return(
-                <div className="container">
-                    <h1>Component will unmount</h1>
-                    <button> Toggle the child component</button>
-                </div>
-            )
-        }
+    }
+    render(){
+        return(
+            <div className="container">
+                <h1>Component will unmount</h1>
+                <button> Toggle the child component</button>
+            </div>
+        )
     }
 }
