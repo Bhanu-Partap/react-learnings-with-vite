727e61883f8dec402f61a4cfba9a43c0d3abb988
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-17 15:18:32 +0530

    Map function usage in return statement

--- my-first-react-app/src/Components/Map.jsx
+++ my-first-react-app/src/Components/Map.jsx
@@ -3,17 +3,25 @@
 export default function Map() {
   const students = ["BMW", "McLaren", "Jeep", "Nissan"];
 
-//====Map function utilization ============//
+  //====Map function utilization ============//
   // students.map((item)=>{
   //     console.warn(item);
   // })
-  
+
 
   return (
     <div className="container">
-      {
+      {/* =========== 1 Method for single line statement================ */}
+      {/* {
         students.map((carBrands) => 
           <h1 key={carBrands}>{carBrands}</h1>
+        )
+      } */}
+      {/* =========== 2 Method for multi-line statement================ */}
+      {
+        students.map((carBrands) => {
+        return  <h1 key={carBrands}>{carBrands}</h1>
+        }
         )
       }
     </div>
