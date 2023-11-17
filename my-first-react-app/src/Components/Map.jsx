fd46df1c089b0a870638a60190212258b984079f
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-17 15:39:29 +0530

    Multiple properties in an array as an object

--- my-first-react-app/src/Components/Map.jsx
+++ my-first-react-app/src/Components/Map.jsx
@@ -1,7 +1,12 @@
 // import React from 'react'/ 
 
 export default function Map() {
-  const students = ["BMW", "McLaren", "Jeep", "Nissan"];
+  // const students = ["BMW", "McLaren", "Jeep", "Nissan"]; // Single entity array
+  const students = [
+    {Name: "BMW", Model:"M5", Year:"2019"},
+    {Name: "Lamborghini", Model:"SVJ", Year:"2019"},
+    {Name: "Nissan", Model:"GTR-R34", Year:"2017"}
+  ]; // Multiple entity array
 
   //====Map function utilization ============//
   // students.map((item)=>{
@@ -20,7 +25,7 @@ export default function Map() {
       {/* =========== 2 Method for multi-line statement================ */}
       {
         students.map((carBrands) => {
-        return  <h1 key={carBrands}>{carBrands}</h1> //the only difference is the return statement , its neended if you want to specify any conditions before the output
+        return  <h1 key={carBrands}>Brand Name :{carBrands}</h1> //the only difference is the return statement , its neended if you want to specify any conditions before the output
         })
       }
     </div>
