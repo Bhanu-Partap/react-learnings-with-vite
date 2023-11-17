388d4ada0698a947b45c4de3d2993e2c6676cb39
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-17 15:10:55 +0530

    Map function usage in return statement

--- my-first-react-app/src/Components/Map.jsx
+++ my-first-react-app/src/Components/Map.jsx
@@ -6,9 +6,10 @@ export default function Map() {
   // students.map((item)=>{
   //     console.warn(item);
   // })
+  
+
   return (
     <div className="container">
-      <h1>Brand names:</h1>
       {
         students.map((carBrands) => {
           <h1 key={carBrands}>{carBrands}</h1>
