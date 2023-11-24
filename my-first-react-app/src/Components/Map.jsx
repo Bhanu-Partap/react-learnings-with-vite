5353c779bb322721278bb5ef631319769761632a
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-11-24 15:08:40 +0530

    h1

--- my-first-react-app/src/Components/Map.jsx
+++ my-first-react-app/src/Components/Map.jsx
@@ -24,11 +24,13 @@ export default function Map() {
         )
       } */}
       {/* =========== 2 Method for multi-line statement================ */}
+      <h1>List is here</h1>
       {
         students.map((carBrands) => {
         return  <h1 key={carBrands.Name}>Brand Name :{carBrands.Name}, {carBrands.Model}, {carBrands.Year}</h1> //the only difference is the return statement , its neended if you want to specify any conditions before the output
         })
       }
+      
     </div>
   )
 }
