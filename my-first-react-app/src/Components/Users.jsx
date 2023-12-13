8f3d9d88ac03bcafa90050574d92cb8f5d08fee1
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-12-13 23:26:05 +0530

    Sending data from child to parent

--- my-first-react-app/src/Components/Users.jsx
+++ my-first-react-app/src/Components/Users.jsx
@@ -1,4 +1,18 @@
 
+//  function Users(props) {
+//   return (
+//     <>
+//         <h1>User {props.data.Name}</h1>
+//     </>
+//     )
+// }
+
+// export default Users
+
+
+// 38 SEND DATA FROM CHILD TO PARENT COMPONENT //
+
+
  function Users(props) {
   return (
     <>
