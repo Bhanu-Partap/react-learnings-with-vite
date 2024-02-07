1f272e3d6f309bd52ba40ed04000d9e912cdef89
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-02-07 15:48:58 +0530

    Params in react router

--- react-router/src/Pages/About.jsx
+++ react-router/src/Pages/About.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import {useParams} from "react"
+// import {useParams} from "react"
 
 export default function About() {
-  const {id = 45} = useParams()
+  // const {id = 45} = useParams()
   return (
     <> 
       <h1>About Page</h1>
-      <Link to="user/45"> Go To {id} Profile</Link> <br />
+      <Link to="user/452"> user</Link> <br />
       {/* <Link to="user/453"> Go To {id} Profile</Link> */}
     </>
   )
