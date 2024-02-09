7c5e3b0938c958ac78f7c8cd50710f0642b2c2de
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-02-09 15:05:20 +0530

    public layout not working

--- react-router/src/Components/Navbar.jsx
+++ react-router/src/Components/Navbar.jsx
@@ -1,21 +1,24 @@
 // import React from 'react'
-import Link  from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">React </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className="navbar-brand" to="/">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                            <Link className="nav-link" to="/about">About</Link>
-                            <Link className="nav-link" to="/contact">Contact</Link>
-                        </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" to="/home">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" to="/about">About</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
--- react-router/src/Router/index.jsx
+++ react-router/src/Router/index.jsx
@@ -1,6 +1,6 @@
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
-import  Home  from '../Pages/Home.jsx'
-import  About  from '../Pages/About.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Home from '../Pages/Home.jsx'
+import About from '../Pages/About.jsx'
 import Page404 from "../Pages/Page404.jsx"
 import User from "../Pages/User.jsx"
 import PublicLayout from "../Layout/PublicLayout/index.jsx"
@@ -8,16 +8,17 @@ import PublicLayout from "../Layout/PublicLayout/index.jsx"
 export default function Routing() {
   return (
     <>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Navigate to="/home" />} />
-                <Route element={<PublicLayout />} />
-                  <Route path='/home' element={<Home />} />
-                  <Route path='/about' element={<About />} />
-                  <Route path='/user/:id' element={<User />} />
-                  <Route path='/*' element={<Page404 />} />
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to="/home" />} />
+          <Route element={<PublicLayout />} >
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/user/:id' element={<User />} />
+            <Route path='/*' element={<Page404 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
