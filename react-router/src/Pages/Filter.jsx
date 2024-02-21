c2a8eb90bb2e4be941fcb2c72bd9216cc2d16ce0
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-02-21 22:45:38 +0530

    filters

---
+++ react-router/src/Pages/Filter.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+import { useSearchParams } from "react-router-dom"
+
+export default function Filter() {
+  return (
+    <>
+        <h2>Filter page</h2>
+    </>
+  )
+}
