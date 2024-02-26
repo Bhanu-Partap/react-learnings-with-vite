a4102d569a32264e481c1710e91134418dbe2339
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-02-26 23:19:30 +0530

    using useNavigate

--- react-router/src/Pages/Home.jsx
+++ react-router/src/Pages/Home.jsx
@@ -3,6 +3,8 @@ export default function Home() {
   return (
     <> 
       <h1>Home Page</h1>
+      <button>Go to Non existing page</button>
+      <button>Go to Non Filter page</button>
     </>
   )
 }
