c190d5988f7862ecdcda35cf88e6068bcf9d2cc3
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-02-27 23:10:04 +0530

    onclick navigation

--- react-router/src/Pages/Home.jsx
+++ react-router/src/Pages/Home.jsx
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <> 
       <h1>Home Page</h1>
-      <button>Go to Non existing page</button>
-      <button onClick= {()=>{<a href="/filter"/> }}>Go to Non Filter page</button>
+      {/* <button>Go to Non existing page</button> */}
+      <button onClick= {()=>{<a href="/filter"></a> }}>Go to  Filter page</button>
     </>
   )
 }
