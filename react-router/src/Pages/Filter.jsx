1a6fc871180c86f5635d4083e103e8151d896ecf
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-03-05 23:36:49 +0530

    positioning

--- react-router/src/Pages/Filter.jsx
+++ react-router/src/Pages/Filter.jsx
@@ -14,12 +14,14 @@ export default function Filter() {
         <h1>Filter page</h1>
         <h3>Age : {age}</h3>
         <h3>City : {city}</h3>
-        <Link to="user">User Page</Link>
-        <Outlet />
+
         {/* USING SETsEARCHpARAMS */}
         <button onClick={()=>setSearchParams({age :22, city: 'Ludhiana'})}>Set the value to the query params </button>
         <input type="number" onChange={(e)=>setSearchParams({age :e.target.value})} placeholder="place Set the value to the query params"/> 
-
+        <br />
+        <br />
+        <Link to="user">User Page</Link>
+        <Outlet />
     </>
   )
 }
