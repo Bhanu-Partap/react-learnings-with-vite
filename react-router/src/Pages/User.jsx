c4a120616ad4f5c3babbecfcf6e8035658600c8f
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-03-22 15:58:38 +0530

    mapping some element

--- react-router/src/Pages/User.jsx
+++ react-router/src/Pages/User.jsx
@@ -1,12 +1,34 @@
-import {useParams, useLocation} from "react-router-dom"
+// import {useParams, useLocation} from "react-router-dom"
+
+// export default function User() {
+//   const {id = 452} = useParams(); 
+//   const location = useLocation();
+//   console.log(location);
+//   return (
+//     <>
+//           <h1>User Page with {id} id</h1>
+
+//     </>
+//   )
+// }
+
+
+
 
 export default function User() {
-  const {id = 452} = useParams(); 
-  const location = useLocation();
-  console.log(location);
+  let users= [
+    {id:1, name:"Bmw", email:"bmw@admin.com"},
+    {id:2, name:"Lamborghini", email:"Lamborghini@admin.com"},
+    {id:3, name:"McLaren", email:"McLaren@admin.com"},
+    {id:4, name:"Cadillac", email:"Cadillac@admin.com"},
+  ]
   return (
     <>
-          <h1>User Page with {id} id</h1>
+    {
+      users.map((item)=>{
+        console.log(item.name); 
+      })
+    }
     </>
   )
 }
